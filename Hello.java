52d7420ba58c3f28b66c472f3aad4b7a410500ab
Nikhar Mehta <nikhar@cigital.com>
2020-06-08 11:28:06 -0400

    new commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package proj1;
+
+public class MyProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello There!");
+		System.out.print("Hello There!");
+		System.out.print("Hello There!");
+
+	}
+
+}
